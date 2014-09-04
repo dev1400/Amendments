b5d53a3fa8a2afed32466ba0a481be072becbb33
Abdul Waheed Mulla <my.hoon.don@gmail.com>
2014-09-04 11:03:51 +0530

    Added table headings & work flow icon in status

--- WebContent/tableview/Component.js
+++ WebContent/tableview/Component.js
@@ -32,6 +32,10 @@ sap.ui.core.UIComponent.extend("sap.ui.demo.myFiori.tableview.Component", {
 
 sap.ui.demo.myFiori.tableview.Component.prototype.createContent = function () {
 	this._rootView = sap.ui.xmlview({ viewName : "sap.ui.demo.myFiori.tableview.Table" });
+	var i18nModel = new sap.ui.model.resource.ResourceModel({
+		bundleUrl : "i18n/messageBundle.properties"
+	});
+	this._rootView.setModel(i18nModel, "i18n");
 	return this._rootView;
 };
 
