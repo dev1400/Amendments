38450c1632180fada52fdc359464ce29b3f39210
Abdul Waheed Mulla <my.hoon.don@gmail.com>
2014-09-11 17:33:12 +0530

    needed

--- WebContent/tableview/Table.controller.js
+++ WebContent/tableview/Table.controller.js
@@ -3,13 +3,10 @@
 sap.ui.controller("sap.ui.demo.myFiori.tableview.Table", {
 
 	onInit: function () {
-		// set explored app's demo model on this sample
+		// using explored app's demo model on this sample
 		var oModel = new sap.ui.model.json.JSONModel("model/products.json");
-		this.getView().setModel(oModel);
+		this.getView().setModel(oModel);		
 		
-		this.getView().setModel(new sap.ui.model.json.JSONModel({
-		      width: (sap.ui.Device.system.phone) ? "50em" : "100em"
-		    }));
 		},
 		handleWorkFlowPress : function(evt) {
 			var context = evt.getSource().getBindingContext();
@@ -30,9 +27,7 @@ sap.ui.controller("sap.ui.demo.myFiori.tableview.Table", {
 			this.getView().addDependent(this._oPopover);
 			this._oPopover.bindElement("/AmendmentsCollection/" + lastChar);
 			// }
-
-			// delay because addDependent will do a async rerendering and the
-			// actionSheet will immediately close without it.
+			
 			var oButton = oEvent.getSource();
 			jQuery.sap.delayedCall(0, this, function() {
 				this._oPopover.openBy(oButton);
