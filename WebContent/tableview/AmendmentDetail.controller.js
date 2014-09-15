0d4f84d9413dbfabe91428d2cc92b921b821b527
Abdul Waheed Mulla <my.hoon.don@gmail.com>
2014-09-15 15:18:06 +0530

    needed

--- WebContent/tableview/AmendmentDetail.controller.js
+++ WebContent/tableview/AmendmentDetail.controller.js
@@ -2,50 +2,47 @@ sap.ui.controller("dia.cmc.contractsinamendment.tableview.AmendmentDetail", {
 
 	onInit: function () {
 		// using explored app's demo model on this sample
-		var oModel = new sap.ui.model.json.JSONModel("model/products.json");
-		this.getView().setModel(oModel);	
-		var bus = sap.ui.getCore().getEventBus();
+	   var oModel = new sap.ui.model.json.JSONModel("model/products.json");
+	   this.getView().setModel(oModel);
+	   
+	   var bus = sap.ui.getCore().getEventBus();
        bus.subscribe("nav", "to", sap.ui.controller("dia.cmc.contractsinamendment.view.App").navToHandler, this);
        bus.subscribe("nav", "back", sap.ui.controller("dia.cmc.contractsinamendment.view.App").navBackHandler, this);
 		
-		},
+	},
 		
-	    navBackHandler : function() {
-	        this.app.back();
-	    },
-		handleWorkFlowPress : function(evt) {
-			/*console.log("handleWorkFlowPress");*/
-			var bindingContext = evt.oSource.getBindingContext();
-			var bus = sap.ui.getCore().getEventBus();
-	        bus.publish("nav", "to", { 
-	            id : "AmendmentFlow",
-	            data : {
+	navBackHandler : function() {
+	   this.app.back();
+    },
+	
+    handleWorkFlowPress : function(evt) {
+	   /*console.log("handleWorkFlowPress");*/
+	   var bindingContext = evt.oSource.getBindingContext();
+	   var bus = sap.ui.getCore().getEventBus();
+	   bus.publish("nav", "to", { 
+	       id : "AmendmentFlow",
+	       data : {
 	                context : bindingContext
-	            }
-	       });				
-			//sap.ui.controller("dia.cmc.contractsinamendment.view.App").to("AmendmentFlow", context);
-			//this.nav.to("AmendmentFlow", context);
-	    },
-
-	    
-		handlePopoverPress : function(oEvent) {
-
-			var local = oEvent.getParameters();
-			var lastChar = local.id;
-			lastChar = lastChar.substr(lastChar.length - 1);
+	       }
+	    });				
+	},
+	handlePopoverPress : function(oEvent) {
+       var local = oEvent.getParameters();
+	   var lastChar = local.id;
+	   lastChar = lastChar.substr(lastChar.length - 1);
 			
-			// create popover
-			// if (!this._oPopover) {
-			this._oPopover = sap.ui.xmlfragment(
+	    // create popover
+		// if (!this._oPopover) {
+		this._oPopover = sap.ui.xmlfragment(
 					"dia.cmc.contractsinamendment.view.AmendmentDescription", this);
-			this.getView().addDependent(this._oPopover);
-			this._oPopover.bindElement("/AmendmentsCollection/" + lastChar);
-			// }
+		this.getView().addDependent(this._oPopover);
+		this._oPopover.bindElement("/AmendmentsCollection/" + lastChar);
+		// }
 			
-			var oButton = oEvent.getSource();
-			jQuery.sap.delayedCall(0, this, function() {
-				this._oPopover.openBy(oButton);
-			});
+		var oButton = oEvent.getSource();
+		jQuery.sap.delayedCall(0, this, function() {
+		this._oPopover.openBy(oButton);
+		});
 		}
 		
 });
